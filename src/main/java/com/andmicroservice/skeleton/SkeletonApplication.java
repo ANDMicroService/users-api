6a669914a874faffe7c1ff85027276cb179a5e8c
msamandi <msamandi@andigital.com>
2017-11-14 13:48:03 +0000

    add logging setting (#5)
    * add logging seting
    
    * add some config clean
    
    * cleaning
    
    * Formatting

--- src/main/java/com/andmicroservice/skeleton/SkeletonApplication.java
+++ src/main/java/com/andmicroservice/skeleton/SkeletonApplication.java
@@ -2,11 +2,16 @@ package com.andmicroservice.skeleton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class SkeletonApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SkeletonApplication.class, args);
+
 	}
 }
--- src/main/java/com/andmicroservice/skeleton/controllers/TestController.java
+++ src/main/java/com/andmicroservice/skeleton/controllers/TestController.java
@@ -1,15 +1,21 @@
 package com.andmicroservice.skeleton.controllers;
 
+import com.andmicroservice.skeleton.SkeletonApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/test")
     @ResponseBody
     public String test() {
+        logger.info("RestController is up and running...");
+
         return "RestController is up and running!";
     }
 }
