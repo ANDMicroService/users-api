49a9750351aef3b38ba666a3fbfb1d9ccb8e94e7
Motiejus Gudenas <lordmotiejus@yahoo.com>
2017-11-15 11:49:59 +0000

    WIP Embedded Mongo

--- src/main/java/com/andmicroservice/users/config/Constants.java
+++ src/main/java/com/andmicroservice/users/config/Constants.java
@@ -8,6 +8,4 @@ public final class Constants {
     //Regex for acceptable logins
     public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
 
-    private Constants() {
-    }
 }
--- src/test/java/com/andmicroservice/users/resource/UserResourceIntTest.java
+++ src/test/java/com/andmicroservice/users/resource/UserResourceIntTest.java
@@ -1,42 +1,66 @@
 package com.andmicroservice.users.resource;
 
 
+import com.andmicroservice.users.representation.UserDTO;
 import com.andmicroservice.users.service.UserServiceImpl;
+import com.andmicroservice.users.util.UserDTOUtils;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {UserResource.class, UserServiceImpl.class})
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserResourceIntTest {
 
-    private MockMvc mockMvc;
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
+    private final HttpHeaders headers = new HttpHeaders();
+
+    @LocalServerPort
+    private int port;
+
+    private String url;
 
     @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        UserResource userResource = new UserResource();
-        this.mockMvc = MockMvcBuilders.standaloneSetup(userResource).build();
+    public void setup() {
+        url = "http://localhost:" + port;
+        headers.setContentType(APPLICATION_JSON_UTF8);
     }
 
-    @Ignore
     @Test
-    public void testApi() throws Exception {
-        this.mockMvc
-                .perform(get("/users").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                        .json("[]"));
+    public void whenPostUser_withValidUser_thenReturnsUser() {
+        UserDTO expectedUserDTO1 = UserDTOUtils.aTestUser();
+
+        ResponseEntity<UserDTO> response = restTemplate.exchange(
+                createURI("/users"),
+                HttpMethod.POST,
+                new HttpEntity<>(expectedUserDTO1, headers),
+                UserDTO.class);
+
+        assertThat(response.getStatusCode(), equalTo(CREATED));
+        assertThat(response.getBody().getId(), equalTo(expectedUserDTO1.getId()));
+    }
+
+    private String createURI(String uri) {
+        return url + uri;
     }
 }
